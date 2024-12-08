a624bcf63c788df4ca6093d57ba5bcadce2c5be9
Andreas Toth <me@ondr.sh>
2024-12-08 09:28:46 +0200

    Add compiler-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,20 @@
 plugins {
-	alias(libs.plugins.kotlin.multiplatform).apply(false)
 	alias(libs.plugins.kotlin.jvm).apply(false)
+	alias(libs.plugins.kotlin.multiplatform).apply(false)
+	alias(libs.plugins.spotless)
+}
+
+allprojects {
+	apply(plugin = "com.diffplug.spotless")
+
+	spotless {
+		kotlin {
+			target("**/*.kt")
+			ktlint()
+		}
+		kotlinGradle {
+			target("**/*.gradle.kts")
+			ktlint()
+		}
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		mavenCentral()
+		mavenLocal()
+	}
+}
+
+dependencyResolutionManagement {
+	repositories {
+		mavenCentral()
+		mavenLocal()
+	}
+}
+
 rootProject.name = "kmcp"
+include("kmcp-compiler")
